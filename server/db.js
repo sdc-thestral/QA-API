196043b504b83c86c83f7baf90bfb816dee17f5e
ctunakan <clairetunakan@gmail.com>
2023-02-16 11:07:06 -0600

    Commit prior to docker usage

--- server/db.js
+++ server/db.js
@@ -1,14 +1,14 @@
 require('dotenv').config;
 const { Client } = require('pg');
 
-const dbServer = new Client({
+const db = new Client({
   user: 'ctunakan',
   host: 'localhost',
   database: process.env.DB_NAME,
   port: 5432,
 });
 
-dbServer.connect();
+db.connect();
 console.log('Connected to PostgreSQL, listening on port 5432');
 
-module.exports = dbServer;
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const { dbServer } = require('./db');
+const db = require('./db');
+const logger = require('./logger');
 
 const app = express();
 
+app.use(cors());
+app.use(logger);
 app.use(express.json());
 
 app.listen(process.env.PORT);
+console.log(`Server listening at http://localhost:${process.env.PORT}`);
