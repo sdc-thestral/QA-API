57a9510be9f6a8d84b4ea33b720805ceebf60c1d
ctunakan <clairetunakan@gmail.com>
2023-02-20 22:22:41 -0600

    Front end and back-end now synced for QA

--- server/dbRoutes.js
+++ server/dbRoutes.js
@@ -1,38 +1,6 @@
 /* eslint-disable radix */
 const db = require('./db');
 
-// questionResponse.rows is an array of question objects
-// need to query each question_id to answers table
-// const responseArr = questionResponse.rows.map((qObject) => {
-//   db.query(`SELECT * FROM answers WHERE question_id = ${qObject.question_id};`)
-//     .then((answerResponse) => {
-//       answerResponse.rows.
-//     });
-// });
-// return responseArr;
-
-// assuming what we need to get question is in req.body, what would we pass to getQuestions
-// const getAllQuestions = (productId, count) => {
-//   const numCount = count || 5;
-//   return db.query(`SELECT * FROM questions WHERE product_id = ${productId} LIMIT ${numCount}`)
-//     .then((questionResponse) => {
-//       const formattedRes = {};
-//       formattedRes.product_id = productId;
-//       formattedRes.results = questionResponse.rows.map((questionObj) => {
-//         const currentQ = {};
-//         currentQ.question_id = questionObj.question_id;
-//         currentQ.question_body = questionObj.question_body;
-//         currentQ.question_date = new Date(JSON.parse(questionObj.question_date)).toString();
-//         currentQ.asker_name = questionObj.asker_name;
-//         currentQ.question_helpfulness = questionObj.question_helpfulness;
-//         currentQ.reported = questionObj.question_reported;
-//         currentQ.answers = getAllAnswers(questionObj.question_id)
-//         return currentQ;
-//       });
-//     })
-//     .catch((err) => console.log('this is error in dbRoutes:', err));
-// };
-
 const getAnswerPhotos = async (answerId) => db.query(`SELECT * FROM answer_photos WHERE answer_id = ${answerId}`)
   .then((response) => {
     const photoArr = response.rows.map((object) => object.photo_url);
@@ -45,7 +13,6 @@ const getAllAnswers = (questionId) => db.query(`SELECT *, ARRAY_AGG(photo_url) p
   WHERE question_id = ${questionId}
   group by answer_id, photo_id`)
   .then((response) => {
-    // console.log(response.rows);
     const answerObj = {};
     response.rows.forEach((obj) => {
       if (answerObj[`${obj.answer_id}`] !== undefined) {
@@ -61,29 +28,33 @@ const getAllAnswers = (questionId) => db.query(`SELECT *, ARRAY_AGG(photo_url) p
         answer.photos = obj.photo_url ? obj.photos : [];
         answerObj[`${answer.id}`] = answer;
       }
-      // console.log(obj);
     });
-    // console.log(answerObj);
     return answerObj;
   })
   .catch((err) => console.log(err));
 
-const getAllQuestions = (productId, count) => {
-  const numCount = count || 5;
-  return db.query(`SELECT *,
+const getAllQuestions = (productId) => db.query(`SELECT *,
   json_build_object('id', answers.answer_id, 'body', answer_body, 'date', answer_date,
   'answerer_name', answerer_name, 'helpfulness', answer_helpfulness,
-  'reported', answer_reported) as answer
+  'reported', answer_reported, 'photos', answer_photos.photo_url) as answer
   from questions
   inner join answers on answers.question_id = questions.question_id
-  inner join answer_photos on answer_photos.answer_id = answers.answer_id
+  full outer join answer_photos on answer_photos.answer_id = answers.answer_id
   WHERE product_id = ${productId}`)
-    .then((questionResponse) => {
-      console.log(questionResponse.rows);
-      const formattedRes = {};
-      formattedRes.product_id = productId;
-      formattedRes.results = [];
-      questionResponse.rows.forEach((questionObj) => {
+  .then((questionResponse) => {
+    const formattedRes = {};
+    formattedRes.product_id = productId;
+    const massObj = {};
+    questionResponse.rows.forEach((questionObj) => {
+      if (massObj[questionObj.question_id] !== undefined) {
+        if (massObj[questionObj.question_id].answers[questionObj.answer.id] !== undefined) {
+          const url = questionObj.photo_url;
+          massObj[questionObj.question_id].answers[questionObj.answer.id].photos.push(url);
+        } else {
+          massObj[questionObj.question_id].answers[questionObj.answer.id] = questionObj.answer;
+          massObj[questionObj.question_id].answers[questionObj.answer.id].photos = [];
+        }
+      } else {
         const currentQ = {};
         currentQ.question_id = questionObj.question_id;
         currentQ.question_body = questionObj.question_body;
@@ -92,31 +63,23 @@ const getAllQuestions = (productId, count) => {
         currentQ.question_helpfulness = questionObj.question_helpfulness;
         currentQ.reported = questionObj.question_reported;
         currentQ.answers = {};
-        currentQ.answers[`${questionObj.answer_id}`] = questionObj.answer;
+        currentQ.answers[questionObj.answer.id] = questionObj.answer;
 
-        formattedRes.results.push(currentQ);
-        // }
-      });
-      return formattedRes;
-    })
-    // .then((responseObj) => {
-    //   const response = {};
-    //   response.product_id = productId;
-    //   response.results = responseObj.results.map((question) => getAllAnswers(question.question_id)
-    //     .then((data) => console.log(data)))
-    //   // getAllAnswers(questionObj.question_id)
-    //   //     .then((data) => data);
-    //   return response;
-    // })
-    .catch((err) => console.log('this is error in dbRoutes:', err));
-};
-
-// const getAnswerPhotos = (answerId) => db.query(`SELECT * FROM answer_photos WHERE answer_id = ${answerId}`)
-//   .then((response) => {
-//     const photoArr = response.rows.map((object) => object.photo_url);
-//     return photoArr;
-//   })
-//   .catch((err) => console.log(err));
+        const photoArr = [];
+        if (questionObj.photo_id) {
+          photoArr.push(currentQ.answers[questionObj.answer.id].photos);
+          currentQ.answers[questionObj.answer.id].photos = photoArr;
+        } else {
+          currentQ.answers[questionObj.answer.id].photos = photoArr;
+        }
+        massObj[questionObj.question_id] = currentQ;
+      }
+    });
+    const results = Object.values(massObj);
+    formattedRes.results = results;
+    return formattedRes;
+  })
+  .catch((err) => console.log('this is error in dbRoutes:', err));
 
 const addQuestion = (productId, body, name, email) => {
   const postTime = new Date();
